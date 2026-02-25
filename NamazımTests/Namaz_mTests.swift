790174f5e36330c2b27ae99742cc2c2a9f6a0899
aptus0 <167253609+aptus0@users.noreply.github.com>
2026-02-25 08:41:05 +0300

    Initial Commit

---
+++ NamazımTests/Namaz_mTests.swift
@@ -0,0 +1,17 @@
+//
+//  Namaz_mTests.swift
+//  NamazımTests
+//
+//  Created by Samet on 25.02.2026.
+//
+
+import Testing
+@testable import Namaz_m
+
+struct Namaz_mTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ NamazımUITests/Namaz_mUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Namaz_mUITestsLaunchTests.swift
+//  NamazımUITests
+//
+//  Created by Samet on 25.02.2026.
+//
+
+import XCTest
+
+final class Namaz_mUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
